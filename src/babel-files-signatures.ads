6ae50591e21c9cd4c77c65814c3d60e9329f42b7
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-08-28 19:23:02 +0000

    Compute the SHA1 signature of a file

---
+++ src/babel-files-signatures.ads
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------
+--  babel-files-signatures -- Signatures calculation
+--  Copyright (C) 2014 Stephane.Carrez
+--  Written by Stephane.Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Babel.Files;
+with Babel.Files.Buffers;
+package Babel.Files.Signatures is
+
+   --  Compute the SHA1 signature of the data stored in the buffer.
+   procedure Sha1 (Buffer : in Babel.Files.Buffers.Buffer;
+                   Result : out Util.Encoders.SHA1.Hash_Array);
+
+end Babel.Files.Signatures;
