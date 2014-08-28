2d6d462928eafa379b886aa57441703ce92723d1
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-08-28 19:25:43 +0000

    File buffer support to load the content of files

---
+++ src/babel-files-buffers.adb
@@ -0,0 +1,34 @@
+-----------------------------------------------------------------------
+--  babel-files-buffers -- File buffer management
+--  Copyright (C) 2014 Stephane.Carrez
+--  Written by Stephane.Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package body Babel.Files.Buffers is
+
+   --  ------------------------------
+   --  Create the buffer pool with a number of pre-allocated buffers of the given maximum size.
+   --  ------------------------------
+   procedure Create_Pool (Into  : in out Buffer_Pools.Pool;
+                          Size  : in Positive;
+                          Count : in Positive) is
+   begin
+      Into.Set_Size (Count);
+      for I in 1 .. Count loop
+         Into.Release (new Buffer (Max_Size => Ada.Streams.Stream_Element_Offset (Size)));
+      end loop;
+   end Create_Pool;
+
+end Babel.Files.Buffers;
