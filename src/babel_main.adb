828c1e03838f9bf40cf4fb2f939cb457b9702a88
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-08-31 15:29:41 +0000

    Update and remove some old code

--- src/babel_main.adb
+++ src/babel_main.adb
@@ -14,19 +14,20 @@ procedure babel_main is
 
    use Ada.Strings.Unbounded;
 
-   Dir         : Babel.Files.Directory;
+   Dir         : Babel.Files.Directory_Type;
    Hex_Encoder : Util.Encoders.Base16.Encoder;
    Exclude     : aliased Babel.Filters.Exclude_Directory_Filter_Type;
    Local       : aliased Babel.Stores.Local.Local_Store_Type;
    Backup      : aliased Babel.Strategies.Default.Default_Strategy_Type;
    Buffers     : aliased Babel.Files.Buffers.Buffer_Pools.Pool;
-
-   procedure Print_Sha (Path : in String;
-                        File : in out Babel.Files.File) is
-      Sha : constant String := Hex_Encoder.Transform (File.SHA1);
-   begin
-      Put_Line (Path & "/" & To_String (File.Name) & " => " & Sha);
-   end Print_Sha;
+   Store       : aliased Babel.Stores.Local.Local_Store_Type;
+--
+--     procedure Print_Sha (Path : in String;
+--                          File : in out Babel.Files.File) is
+--        Sha : constant String := Hex_Encoder.Transform (File.SHA1);
+--     begin
+--        Put_Line (Path & "/" & To_String (File.Name) & " => " & Sha);
+--     end Print_Sha;
 
    procedure Do_Backup (Count : in Positive) is
       Workers : Babel.Strategies.Workers.Worker_Type (Count);
@@ -38,16 +39,18 @@ procedure babel_main is
 begin
    Exclude.Add_Exclude (".svn");
    Exclude.Add_Exclude ("obj");
-   Babel.Files.Buffers.Create_Pool (Buffers, 10, 1_000_000);
+   Babel.Files.Buffers.Create_Pool (Into => Buffers, Count => 10, Size => 1_000_000);
+   Store.Set_Root_Directory ("/tmp/babel-store");
+   Local.Set_Root_Directory (".");
    Backup.Set_Filters (Exclude'Unchecked_Access);
-   Backup.Set_Stores (Local'Unchecked_Access, Local'Unchecked_Access);
+   Backup.Set_Stores (Read => Local'Unchecked_Access, Write => Store'Unchecked_Access);
    Backup.Set_Buffers (Buffers'Unchecked_Access);
 
    Put_Line ("Size: " & Natural'Image (Ada.Directories.File_Size'Size));
    Do_Backup (2);
 --     Bkp.Files.Scan (".", Dir);
 --     Bkp.Files.Iterate_Files (".", Dir, 10, Print_Sha'Access);
-   Put_Line ("Total size: " & Ada.Directories.File_Size'Image (Dir.Tot_Size));
-   Put_Line ("File count: " & Natural'Image (Dir.Tot_Files));
-   Put_Line ("Dir count:  " & Natural'Image (Dir.Tot_Dirs));
+--     Put_Line ("Total size: " & Ada.Directories.File_Size'Image (Dir.Tot_Size));
+--     Put_Line ("File count: " & Natural'Image (Dir.Tot_Files));
+--     Put_Line ("Dir count:  " & Natural'Image (Dir.Tot_Dirs));
 end babel_main;
