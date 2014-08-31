cb8a71a4b6e8ad303c18bf95ee622d57f2156663
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-08-31 15:27:47 +0000

    Moved the Fifo and and File_Queue in the Babel.Files.Queues package

---
+++ src/babel-files-queues.ads
@@ -0,0 +1,68 @@
+-----------------------------------------------------------------------
+--  bkp-files -- File and directories
+--  Copyright (C) 2014 Stephane.Carrez
+--  Written by Stephane.Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
+with Ada.Calendar;
+with Ada.Directories;
+with Ada.Containers.Vectors;
+with Util.Encoders.SHA1;
+with Util.Concurrent.Fifos;
+with Util.Strings.Vectors;
+with ADO;
+with Babel.Base.Models;
+package Babel.Files.Queues is
+
+   --  package Babel.Strategies.Flow/Optimize/Small/Larges/Default/Immediate/Simple/Pipeline/Serial
+   --  File name
+   --  Directory info
+   --  File ID or null
+   --  File size         => sha1, file status, backup
+   package File_Fifo is new Util.Concurrent.Fifos (Element_Type     => File_Type,
+                                                   Default_Size     => 100,
+                                                   Clear_On_Dequeue => True);
+   --
+   --     procedure Execute (Backup : in out Small_Files_Strategy;
+   --                        Queue  : in out File_Queue) is
+   --     begin
+   --        loop
+   --           Queue.Dequeue (File, 1.0);
+   --           --  load file
+   --           --  compute sha1
+   --           --  if modified then backup file
+   --        end loop;
+   --
+   --     exception
+   --        when File_Fifo.Timeout =>
+   --           null;
+   --     end Execute;
+
+   type File_Queue is limited new File_Container with record
+      Queue       : File_Fifo.Fifo;
+      Directories : Util.Strings.Vectors.Vector;
+   end record;
+
+   overriding
+   procedure Add_File (Into    : in out File_Queue;
+                       Path    : in String;
+                       Element : in File);
+
+   overriding
+   procedure Add_Directory (Into : in out File_Queue;
+                            Path : in String;
+                            Name : in String);
+
+end Babel.Files.Queues;
