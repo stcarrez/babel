7d9e3fa1896d384c6fdc667711343fe87d08b613
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-05-08 11:27:21 +0200

    Change to a generic package and define a table of strategy objects Declare the Configure procedure

--- src/babel-strategies-workers.ads
+++ src/babel-strategies-workers.ads
@@ -15,23 +15,34 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+generic
+   type Worker_Strategy is limited new Babel.Strategies.Strategy_Type with private;
 package Babel.Strategies.Workers is
 
    type Worker_Type (Count : Positive) is limited private;
 
-   procedure Start (Worker   : in out Worker_Type;
-                    Strategy : in Babel.Strategies.Strategy_Type_Access);
+   procedure Configure (Worker  : in out Worker_Type;
+                        Process : not null access procedure (S : in out Worker_Strategy));
+
+   procedure Start (Worker   : in out Worker_Type);
+
+   procedure Finish (Worker   : in out Worker_Type;
+                     Database : in out Babel.Base.Database'Class);
 
 private
 
    task type Worker_Task is
       entry Start (Strategy : in Babel.Strategies.Strategy_Type_Access);
+
+      entry Finish (Database : in out Babel.Base.Database'Class);
    end Worker_Task;
 
    type Worker_Task_Array is array (Positive range <>) of Worker_Task;
+   type Strategy_Array is array (Positive range <>) of aliased Worker_Strategy;
 
    type Worker_Type (Count : Positive) is limited record
-      Workers : Worker_Task_Array (1 .. Count);
+      Workers    : Worker_Task_Array (1 .. Count);
+      Strategies : Strategy_Array (1 .. Count);
    end record;
 
 end Babel.Strategies.Workers;
