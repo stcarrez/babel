cd1cc333103f23989721997507d14f65b4574955
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-05-13 23:00:17 +0200

    Declare Read_File, Write_File and Set_Buffers operation

--- src/babel-stores-local.ads
+++ src/babel-stores-local.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  babel-stores-local -- Store to access local files
---  Copyright (C) 2014 Stephane.Carrez
+--  Copyright (C) 2014, 2015, 2016 Stephane.Carrez
 --  Written by Stephane.Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +18,7 @@
 with Ada.Strings.Unbounded;
 with Util.Streams.Raw;
 
+with Babel.Files.Buffers;
 package Babel.Stores.Local is
 
    type Local_Store_Type is new Babel.Stores.Store_Type with private;
@@ -31,6 +32,19 @@ package Babel.Stores.Local is
    procedure Open_File (Store  : in out Local_Store_Type;
                         Path   : in String;
                         Stream : out Babel.Streams.Stream_Access);
+
+   --  Open a file in the store to read its content with a stream.
+   overriding
+   procedure Read_File (Store  : in out Local_Store_Type;
+                        Path   : in String;
+                        Stream : out Babel.Streams.Refs.Stream_Ref);
+
+   --  Write a file in the store with a stream.
+   overriding
+   procedure Write_File (Store  : in out Local_Store_Type;
+                         Path   : in String;
+                         Stream : in Babel.Streams.Refs.Stream_Ref;
+                         Mode   : in Util.Systems.Types.mode_t);
 
    overriding
    procedure Read (Store : in out Local_Store_Type;
@@ -56,10 +70,15 @@ package Babel.Stores.Local is
    procedure Set_Root_Directory (Store : in out Local_Store_Type;
                                  Path  : in String);
 
+   --  Set the buffer pool to be used by local store.
+   procedure Set_Buffers (Store   : in out Local_Store_Type;
+                          Buffers : in Babel.Files.Buffers.Buffer_Pool_Access);
+
 private
 
    type Local_Store_Type is new Babel.Stores.Store_Type with record
       Root_Dir : Ada.Strings.Unbounded.Unbounded_String;
+      Pool     : Babel.Files.Buffers.Buffer_Pool_Access;
    end record;
 
 end Babel.Stores.Local;
